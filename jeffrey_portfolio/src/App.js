8fa543c2b2d9b9a3b7fcff7f7c3945326208b22d
J-Striker <46310215+J-Striker@users.noreply.github.com>
2020-08-12 14:40:58 +0400

    entrainement

---
+++ jeffrey_portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Welcome from './Welcome.js';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <Welcome name="jackson"/>
+        );
+    }
+}
+ 
+export default App;
