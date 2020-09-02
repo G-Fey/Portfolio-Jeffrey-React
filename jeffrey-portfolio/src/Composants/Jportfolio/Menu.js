7838284afd02f481177db32f2c778a873a7258db
J-Striker <46310215+J-Striker@users.noreply.github.com>
2020-09-03 00:29:14 +0400

    jeffrey_portfolio v0.3
    Correction du probleme avec Map et les props.

--- jeffrey-portfolio/src/Composants/Jportfolio/Menu.js
+++ jeffrey-portfolio/src/Composants/Jportfolio/Menu.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
-import Datajson from '../../data/data_portfolio.json'
 
 
 class Menu extends Component {
-    constructor(props) {
-        super(props);
+
+    showdata = (datas) =>{
+    
     }
 
-
     render() {
-        return (
-            <nav>
-                <ul>
-                    {this.props.data.map(data => (<li key={data.id}> <a href={data.lien_element}><i href={data.icone_element}></i>{data.nom_element}</a></li>)) }
 
-                </ul>
-                
-            </nav>
-        );
+        let datas = this.props.data
+
+        if(datas){
+
+            return (
+                <nav>
+                    <ul>
+                        {  
+                            datas.map(data =>
+                            {
+                                return(
+                                    <li key={data.id}> 
+                                        <a href={data.lien_element}><i href={data.icone_element}></i>{data.nom_element}</a>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul> 
+                </nav>
+            );
+
+        }else{
+            return (
+                <nav>
+                   
+                </nav>
+            );
+        }
     }
 }
 
