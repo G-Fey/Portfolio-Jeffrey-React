ab4aca620b921b2aa97527e03465518b3a18678a
J-Striker <46310215+J-Striker@users.noreply.github.com>
2020-10-02 00:04:55 +0400

    Ajout de la navbar
    Ajout :
    - icone
    - navbar retractable
    - nom des section

--- jeffrey-portfolio/src/App.js
+++ jeffrey-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 import Jportfolio from './Composants/Jportfolio/Jportfolio';
 
 function App() {
