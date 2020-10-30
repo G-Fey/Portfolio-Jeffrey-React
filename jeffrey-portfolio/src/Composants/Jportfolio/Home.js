47d5ebf569e55936402d43c6e6bb9c41d88ebd19
J-Striker <46310215+J-Striker@users.noreply.github.com>
2020-10-30 21:26:08 +0400

    Ajoute sur la parti home
    Ajout bouton start
    H1, H2

--- jeffrey-portfolio/src/Composants/Jportfolio/Home.js
+++ jeffrey-portfolio/src/Composants/Jportfolio/Home.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 import './../../scss/Home.scss';
-import { FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { GrMail } from 'react-icons/gr';
 
 class home extends Component {
     render() {
         return (
-            <section>
-                <h1>Jeffrey ANAVILLELA</h1>
-                <h2>Aka G-Fey</h2>
-                <h2>Développeur Web, Designer Web, Illustrateur, Monteur vidéo, FX, Technicien informatique</h2>
-                <div>
-                    <a className="btn btn-lg btn-gradient flat linkedin" href="https://www.linkedin.com/in/jeffrey-anavillela-202398153/" target="blank" role="button"><div className="taille"><FaLinkedinIn color="white" size="0.8em"/></div></a>
-                    <a className="btn btn-lg btn-gradient flat git" href="https://github.com/J-Striker" target="blank" role="button"><div className="taille"><FaGithub color="white" size="0.8em" /></div></a>
-                    <a className="btn btn-lg btn-gradient flat mail" href="mailto:janavillela@laposte.net" role="button"><div className="taille"><GrMail color="white" size="0.8em"/></div></a>
-                    <a className="btn btn-lg btn-gradient flat face" href="https://www.facebook.com/jeffrey.anavillela" target="blank" role="button"><div className="taille"><FaFacebookF color="white" size="0.8em"/></div></a>
-                </div> 
-                <button>
-                    Start
-                </button>
-                <svg>
+            <section className="home container-fluide ">
+                <div className="p-5 ml-5">
+                    <h1 >Jeffrey ANAVILLELA</h1>
+                    <h2 className="pb-3">Aka G-Fey</h2>
+                    <h2>Développeur Web, Designer Web, Illustrateur, Monteur vidéo, FX, Technicien informatique</h2>
+                    <div class="box-2 d-flex justify-content-center">
+                        <div class="btn btn-two">
+                            <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                                <polygon points="25 15, 50 50, 0 50"/>
+                            </svg>
+                            <span>Start</span>
+                        </div>
+                    </div>
+                    <svg>
 
-                </svg>
+                    </svg>
+                </div>
             </section>
         );
     }
